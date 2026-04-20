90126b0c451947d764c613acae84fffb8e35751a
rrassya19-bit <rrassys19@gmail.com>
2026-04-20 14:34:47 +0700

    Finalisasi - perbaikan tampilan dan komentar

--- ParadigmaOOP.cpp
+++ ParadigmaOOP.cpp
@@ -1,27 +1,32 @@
 #include <iostream>
 using namespace std;
 
-// Class Mahasiswa untuk merepresentasikan data mahasiswa
 class Mahasiswa {
 public:
-    int nim;
-    string nama;
-    float nilai;
+    int nim;     
+    string nama;  
+    float nilai;  
 
+    // Method untuk menampilkan semua data mahasiswa
     void printData() {
+        cout << "=== DATA MAHASISWA ===" << endl;
         cout << "NIM   : " << nim << endl;
         cout << "Nama  : " << nama << endl;
         cout << "Nilai : " << nilai << endl;
+        cout << "======================" << endl;
     }
 };
 
 int main() {
+    // Membuat objek mahasiswa
     Mahasiswa mhs;
     
+    // Mengisi data mahasiswa
     mhs.nim = 20250140157;
     mhs.nama = "Ahmad Rassya Maulana";
     mhs.nilai = 95.5;
     
+    // Menampilkan data
     mhs.printData();
     
     return 0;
