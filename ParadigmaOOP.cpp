a8bcc38dfdee3b12f7e28765526cff424b6f94e5
rrassya19-bit <rrassys19@gmail.com>
2026-04-20 14:31:52 +0700

    Menambahkan method printData()

--- ParadigmaOOP.cpp
+++ ParadigmaOOP.cpp
@@ -6,6 +6,12 @@ public:
     int nim;
     string nama;
     float nilai;
+
+    void printData() {
+        cout << "NIM   : " << nim << endl;
+        cout << "Nama  : " << nama << endl;
+        cout << "Nilai : " << nilai << endl;
+    }
 };
 
 int main() {
